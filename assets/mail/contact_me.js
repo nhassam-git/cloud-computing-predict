0e56e348cb3ec9f0b2bd99339841ad2b14d878d0
nhassam-git <nisaarah786@gmail.com>
2021-07-01 16:38:34 +0200

    Update endpoint

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://qzxioxwtg6.execute-api.eu-west-1.amazonaws.com/postrequest",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
